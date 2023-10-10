bb03aa02e0f0e26eee1601a3757e7d4f30be4e27
jordannguyen321 <144298368+jordannguyen321@users.noreply.github.com>
2023-10-09 22:12:48 -0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,15 +1,15 @@
-def merge_list(list1, list2):
-    if not (isinstance(list1, list) and isinstance(list2, list)):
+def merge_list(l1, l2):
+    if not (isinstance(l1, list) and isinstance(l2, list)):
         raise TypeError("Both inputs must be lists")
     
     # Merges both of the lists
-    merged_list = list1 + list2
+    mergedList = l1 + l2
 
     #Simple bubble sort to sort the lists
-    x = len(merged_list)
+    x = len(mergedList)
     for i in range(x - 1):
         for j in range(0, x - i - 1):
-            if merged_list[j] > merged_list[j + 1]:
-                merged_list[j], merged_list[j + 1] = merged_list[j + 1], merged_list[j]
+            if mergedList[j] > mergedList[j + 1]:
+                mergedList[j], mergedList[j + 1] = mergedList[j + 1], mergedList[j]
 
-    return merged_list
+    return mergedList
