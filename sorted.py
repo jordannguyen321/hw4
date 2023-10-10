d1cfcee947582de002a985c31eacbf2c5f7e5fd1
jordannguyen321 <144298368+jordannguyen321@users.noreply.github.com>
2023-10-09 19:12:58 -0700

    Create sorted.py

---
+++ sorted.py
@@ -0,0 +1,9 @@
+def reverse_sort_dictionary(inDict):
+    if not isinstance(inDict, dict):
+        raise TypeError("Input is not a dictionary")
+    
+    sorted_items = sorted(inDict.items(), key=lambda x: x[0], reverse=True)
+    
+    result = [(name, data[0]) for name, data in sorted_items]
+    
+    return result
