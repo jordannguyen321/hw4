92bbd598a432a18fd886afdca99a5df497dee114
jordannguyen321 <144298368+jordannguyen321@users.noreply.github.com>
2023-10-09 18:55:59 -0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,15 +1,25 @@
 def merge_list(list1, list2):
-    if not isinstance(list1, list) or not isinstance(list2, list):
+    if not (isinstance(list1, list) and isinstance(list2, list)):
         raise TypeError("Both inputs must be lists")
-
-    # Merges both lists into one sorted list
+    
+    # Merges both of the lists
     merged_list = list1 + list2
 
-    # Bubble sort 
-    n = len(merged_list)
-    for i in range(n - 1):
-        for j in range(0, n - i - 1):
+    #Simple bubble sort to sort the lists
+    x = len(merged_list)
+    for i in range(x - 1):
+        for j in range(0, x - i - 1):
             if merged_list[j] > merged_list[j + 1]:
                 merged_list[j], merged_list[j + 1] = merged_list[j + 1], merged_list[j]
 
     return merged_list
+
+if __name__ == "__main__":
+    main:
+    try:
+        list1 = [1, 5, 3, 7]
+        list2 = [6, 2, 4]
+        result = merge_list(list1, list2)
+        print(result)
+    except TypeError as y:
+        print(f"Error: {y}")
