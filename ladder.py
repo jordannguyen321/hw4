1baad9e5a23b4d482ea5c12854a10190451c1896
jordannguyen321 <144298368+jordannguyen321@users.noreply.github.com>
2023-10-09 18:44:57 -0700

    Update ladder.py

--- ladder.py
+++ ladder.py
@@ -13,11 +13,3 @@ def my_steps(n):
         ways[i] = ways[i - 1] + ways[i - 2]
 
     return ways[n]
-
-if __name__ == "__main__":
-    try:
-        n = int(input("Enter the ladder height 1-25: "))
-        result = my_steps(n)
-        print(f"The total number of ways to climb the ladder: {result}")
-    except ValueError as x:
-        print(f"Error: {x}")
